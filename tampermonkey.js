20a6ac5aea5a2038653f6e9212e55168fc74ccd3
KingNotNgu <th491725@gmail.com>
2025-05-22 22:24:39 +0700

    Create kingdepzai.js

---
+++ tampermonkey.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         UGPhone Login Tool
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Ugphone Login
+// @author       King 👑
+// @require      https://github.com/KingNotNgu/localstorage/raw/refs/heads/main/ugphone_login.js
+// @match        https://www.ugphone.com/*
+// @match        https://*.ugphone.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-end
+// @icon         https://image.kingdev.sbs/image/king.png?s=48&v=4
+// ==/UserScript==
